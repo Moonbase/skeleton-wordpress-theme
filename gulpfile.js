a17e27e8d0b4e83481fc49ad188c8fe24801b7e3
Ward van Teijlingen <ward@moonbase.co>
2016-12-24 22:46:52 +0100

    Updated Babelify and Browserify.

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,15 @@
 var package     = require('./package.json');
 var gulp        = require('gulp');
-var browserify  = require('gulp-browserify');
 var concat      = require('gulp-concat-util');
 var sass        = require('gulp-sass');
-var babel       = require('gulp-babel');
 var uglify      = require('gulp-uglify');
 var sourcemaps  = require('gulp-sourcemaps');
+var browserify  = require('browserify');
+var babelify    = require('babelify');
+var source      = require('vinyl-source-stream');
+var buffer      = require('vinyl-buffer');
 
-// Specify vendor Javascript and CSSthat you cannot use through NPM.
+// Specify vendor Javascript and CSS that you cannot use through NPM.
 // You need to restart Gulp after changing these variables.
 var vendorJS = [
 	// 'assets/vendor/some-library/some-library.js',
@@ -24,17 +26,21 @@ var metaData =  '/*\nTheme Name: ' + package.name +
                   '\n*/\n';
 
 gulp.task('compile', function() {
-	gulp.src('assets/stylesheets/style.scss')
-    .pipe(sourcemaps.init())
-		.pipe(sass().on('error', sass.logError))
-		.pipe(concat.header(metaData))
-    .pipe(sourcemaps.write())
-		.pipe(gulp.dest('.'));
+  gulp.src('assets/stylesheets/style.scss')
+  .pipe(sourcemaps.init())
+  .pipe(sass().on('error', sass.logError))
+  .pipe(concat.header(metaData))
+  .pipe(sourcemaps.write())
+  .pipe(gulp.dest('.'));
 
-	gulp.src('assets/javascript/main.js')
-    .pipe(sourcemaps.init())
-		.pipe(babel({ presets: ['latest'] }))
-    .pipe(browserify())
+  browserify({
+    entries: './assets/javascript/main.js'
+  }).transform(babelify, {
+    presets: ['latest']
+  }).bundle()
+    .pipe(source('main.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({ loadMaps: true }))
 		.pipe(uglify())
     .pipe(sourcemaps.write())
 		.pipe(gulp.dest('.'));
@@ -48,7 +54,7 @@ gulp.task('compileVendor', function() {
     .pipe(sourcemaps.write())
 		.pipe(gulp.dest('.'));
 
-	gulp.src(vendorCSS, {base: 'assets/vendor/'})
+  gulp.src(vendorCSS, {base: 'assets/vendor/'})
     .pipe(sourcemaps.init())
 		.pipe(concat('vendor.css'))
     .pipe(sourcemaps.write())
@@ -58,5 +64,5 @@ gulp.task('compileVendor', function() {
 gulp.task('build', ['compile', 'compileVendor']);
 
 gulp.task('watch', ['build'], function() {
-	gulp.watch('assets/**/*', ['compile']);
+	gulp.watch('assets/**/*', ['build']);
 });
