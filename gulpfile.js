c0ec0f540bc020179afd4bb76509d376e6a30e89
Ward van Teijlingen <ward@moonbase.co>
2017-01-17 13:25:14 +0100

    Updated gulpfile.

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,14 @@
-var package     = require('./package.json');
-var gulp        = require('gulp');
-var concat      = require('gulp-concat-util');
-var sass        = require('gulp-sass');
-var uglify      = require('gulp-uglify');
-var sourcemaps  = require('gulp-sourcemaps');
-var browserify  = require('browserify');
-var babelify    = require('babelify');
-var source      = require('vinyl-source-stream');
-var buffer      = require('vinyl-buffer');
+var package      = require('./package.json');
+var gulp         = require('gulp');
+var concat       = require('gulp-concat-util');
+var sass         = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var uglify       = require('gulp-uglify');
+var sourcemaps   = require('gulp-sourcemaps');
+var browserify   = require('browserify');
+var babelify     = require('babelify');
+var source       = require('vinyl-source-stream');
+var buffer       = require('vinyl-buffer');
 
 // Specify vendor Javascript and CSS that you cannot use through NPM.
 // You need to restart Gulp after changing these variables.
@@ -25,14 +26,23 @@ var metaData =  '/*\nTheme Name: ' + package.name +
                   '\nVersion: ' + package.version +
                   '\n*/\n';
 
-gulp.task('compile', function() {
+gulp.task('compile:stylesheets', function() {
   gulp.src('assets/stylesheets/style.scss')
-  .pipe(sourcemaps.init())
-  .pipe(sass().on('error', sass.logError))
-  .pipe(concat.header(metaData))
-  .pipe(sourcemaps.write())
-  .pipe(gulp.dest('.'));
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({ browsers: ['last 2 versions'], cascade: false }))
+    .pipe(concat.header(metaData))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('.'));
 
+  gulp.src(vendorCSS, {base: 'assets/vendor/'})
+    .pipe(sourcemaps.init())
+		.pipe(concat('vendor.css'))
+    .pipe(sourcemaps.write())
+		.pipe(gulp.dest('.'));
+});
+
+gulp.task('compile:javascript', function() {
   browserify({
     entries: './assets/javascript/main.js'
   }).transform(babelify, {
@@ -44,24 +54,16 @@ gulp.task('compile', function() {
 		.pipe(uglify())
     .pipe(sourcemaps.write())
 		.pipe(gulp.dest('.'));
-});
 
-gulp.task('compileVendor', function() {
-	gulp.src(vendorJS, {base: 'assets/vendor/'})
+  gulp.src(vendorJS, {base: 'assets/vendor/'})
     .pipe(sourcemaps.init())
 		.pipe(concat('vendor.js'))
 		.pipe(uglify())
     .pipe(sourcemaps.write())
 		.pipe(gulp.dest('.'));
-
-  gulp.src(vendorCSS, {base: 'assets/vendor/'})
-    .pipe(sourcemaps.init())
-		.pipe(concat('vendor.css'))
-    .pipe(sourcemaps.write())
-		.pipe(gulp.dest('.'));
 });
 
-gulp.task('build', ['compile', 'compileVendor']);
+gulp.task('build', ['compile:stylesheets', 'compile:javascript']);
 
 gulp.task('watch', ['build'], function() {
 	gulp.watch('assets/**/*', ['build']);
