beeff2b9bd373d3d6d567f02fe11410b0a25edc7
Ward van Teijlingen <ward@moonbase.co>
2017-03-28 15:01:01 +0200

    Added support for Yarn.

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # Set up skeleton branch
+echo "== Configuring skeleton and master branch =="
 git branch -m master skeleton
 git remote rename origin skeleton-wordpress-theme
 git remote set-url --push skeleton-wordpress-theme no-pushing
@@ -10,10 +11,19 @@ git branch -u skeleton-wordpress-theme/master
 git checkout -b master
 
 # Set up new remote
-echo -n "Enter a new Git repository origin: "
+echo "== Configuring remotes =="
+echo -n "Enter a new Git repository origin:"
 read neworigin
 if [ -n "$neworigin" ]; then
   git remote add origin "$neworigin"
 fi
 
-echo "Setup complete. Pull the 'skeleton-wordpress-theme' branch, then merge it into 'master' to update from the skeleton."
+echo "== Installing NPM dependencies =="
+yarn install
+
+echo "== Building assets =="
+yarn run build
+
+echo "== Setup complete =="
+echo "- You can remove the setup script and 'Creating a project' section from the readme."
+echo "- To update from the skeleton pull the 'skeleton-wordpress-theme' branch, then merge it into 'master'."
